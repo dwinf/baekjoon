a878e2c21a22251fba5697a45142a97ec263032f
DaeChan Kim <dwinf88@gmail.com>
2021-01-24 20:27:08 +0900

    first

---
+++ io/10869.py
@@ -0,0 +1,8 @@
+#import math
+a, b = input().split()
+print(int(a) + int(b))
+print(int(a) - int(b))
+print(int(a) * int(b))
+#print(math.floor(int(a) / int(b)))
+print(int(a) // int(b))
+print(int(a) % int(b))
